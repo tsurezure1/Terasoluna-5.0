1080d44729d928a3e5d17f8f8d6a783123afe0ec
tsurezure1 <kota_wat1106@hotmail.com>
2016-04-08 02:09:30 +0900

    初回コミット

---
+++ todo/src/main/java/todo/domain/repository/todo/TodoRepository.java
@@ -0,0 +1,16 @@
+package todo.domain.repository.todo;
+
+import java.util.Collection;
+import todo.domain.model.Todo;
+
+public interface TodoRepository {
+    Todo findOne(String todoId);
+
+    Collection<Todo> findAll();
+
+    void create(Todo todo);
+
+    boolean update(Todo todo);
+
+    void delete(Todo todo);
+}
